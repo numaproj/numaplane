993908c765b671d94d6ffb1d1db5e4ae6bc94ccc
Ali Ibrahim <a49ibrahim@gmail.com>
2024-06-07 08:55:23 -0700

    feat: Add test for isbsservice
    Signed-off-by: Ali Ibrahim <a49ibrahim@gmail.com>

--- internal/controller/isbservicerollout_controller_test.go
+++ internal/controller/isbservicerollout_controller_test.go
@@ -17,74 +17,137 @@ limitations under the License.
 package controller
 
 import (
-	"context"
+    "context"
+    "time"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
+    "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/types"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	apiv1 "github.com/numaproj/numaplane/pkg/apis/numaplane/v1alpha1"
+    apiv1 "github.com/numaproj/numaplane/pkg/apis/numaplane/v1alpha1"
 )
 
 var _ = Describe("ISBServiceRollout Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+    const (
+        namespace           = "default"
+        isbServiceRolloutName = "isbservicerollout-test"
+        timeout             = 10 * time.Second
+        interval            = 250 * time.Millisecond
+    )
 
-		ctx := context.Background()
+    ctx := context.Background()
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		isbservicerollout := &apiv1.ISBServiceRollout{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind ISBServiceRollout")
-			err := k8sClient.Get(ctx, typeNamespacedName, isbservicerollout)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &apiv1.ISBServiceRollout{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+    isbServiceRollout := &apiv1.ISBServiceRollout{
+			ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      isbServiceRolloutName,
+			},
+			Spec: apiv1.ISBServiceRolloutSpec{
+					InterStepBufferService: runtime.RawExtension{
+							Raw: []byte(`{
+									"apiVersion": "v1",
+									"kind": "Service",
+									"metadata": {
+											"name": "my-service"
+									},
+									"spec": {
+											"selector": {
+													"app": "MyApp"
+											},
+											"ports": [{
+													"protocol": "TCP",
+													"port": 80,
+													"targetPort": 9376
+											}]
+									}
+							}`),
 					},
-					// TODO(user): Specify other spec details if needed.
-					Spec: apiv1.ISBServiceRolloutSpec{
-						InterStepBufferService: runtime.RawExtension{
-							Raw: []byte(`{"field":"val"}`),
-						},
-					},
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+			},
+	}
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &apiv1.ISBServiceRollout{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+    resourceLookupKey := types.NamespacedName{Name: isbServiceRolloutName, Namespace: namespace}
 
-			By("Cleanup the specific resource instance ISBServiceRollout")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		// It("should successfully reconcile the resource", func() {
-		// 	By("Reconciling the created resource")
-		// 	controllerReconciler := &ISBServiceRolloutReconciler{
-		// 		client:     k8sClient,
-		// 		scheme:     k8sClient.Scheme(),
-		// 		restConfig: cfg,
-		// 	}
+    Context("When applying a ISBServiceRollout spec", func() {
+        It("Should create the ISBServiceRollout if it does not exist", func() {
+            Expect(k8sClient.Create(ctx, isbServiceRollout)).Should(Succeed())
 
-		// 	_, err := controllerReconciler.Reconcile(ctx, sigsReconcile.Request{
-		// 		NamespacedName: typeNamespacedName,
-		// 	})
-		// 	Expect(err).NotTo(HaveOccurred())
-		// 	// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-		// 	// Example: If you expect a certain status condition after reconciliation, verify it here.
-		// })
-	})
+            createdResource := &apiv1.ISBServiceRollout{}
+            Eventually(func() bool {
+                err := k8sClient.Get(ctx, resourceLookupKey, createdResource)
+                return err == nil
+            }, timeout, interval).Should(BeTrue())
+
+            By("Verifying the content of the ISBServiceRollout spec field")
+            Expect(createdResource.Spec).Should(Equal(isbServiceRollout.Spec))
+        })
+
+        It("Should update the ISBServiceRollout", func() {
+            By("updating the ISBServiceRollout")
+
+            currentISBServiceRollout := &apiv1.ISBServiceRollout{}
+            Expect(k8sClient.Get(ctx, resourceLookupKey, currentISBServiceRollout)).ToNot(HaveOccurred())
+
+            // Update the spec here
+						currentISBServiceRollout.Spec.InterStepBufferService = runtime.RawExtension{
+							Raw: []byte(`{
+									"apiVersion": "v1",
+									"kind": "Service",
+									"metadata": {
+											"name": "my-updated-service"
+									},
+									"spec": {
+											"selector": {
+													"app": "MyUpdatedApp"
+											},
+											"ports": [{
+													"protocol": "TCP",
+													"port": 8080,
+													"targetPort": 9377
+											}]
+									}
+							}`),
+						}
+
+            Expect(k8sClient.Update(ctx, currentISBServiceRollout)).ToNot(HaveOccurred())
+
+            By("Verifying the content of the ISBServiceRollout spec field")
+						Eventually(func() apiv1.ISBServiceRolloutSpec {
+							updatedResource := &apiv1.ISBServiceRollout{}
+							_ = k8sClient.Get(ctx, resourceLookupKey, updatedResource)
+							return updatedResource.Spec
+					}, timeout, interval).Should(Equal(currentISBServiceRollout.Spec))
+        })
+
+        It("Should delete the ISBServiceRollout", func() {
+            Expect(k8sClient.Delete(ctx, &apiv1.ISBServiceRollout{
+                ObjectMeta: isbServiceRollout.ObjectMeta,
+            })).Should(Succeed())
+
+            deletedResource := &apiv1.ISBServiceRollout{}
+            Eventually(func() bool {
+                err := k8sClient.Get(ctx, resourceLookupKey, deletedResource)
+                return errors.IsNotFound(err)
+            }, timeout, interval).Should(BeTrue())
+        })
+    })
+
+    Context("When applying an invalid ISBServiceRollout spec", func() {
+        It("Should not create the ISBServiceRollout", func() {
+            Expect(k8sClient.Create(ctx, &apiv1.ISBServiceRollout{
+                Spec: isbServiceRollout.Spec,
+            })).To(HaveOccurred())
+
+            Expect(k8sClient.Create(ctx, &apiv1.ISBServiceRollout{
+                ObjectMeta: isbServiceRollout.ObjectMeta,
+            })).To(HaveOccurred())
+
+            Expect(k8sClient.Create(ctx, &apiv1.ISBServiceRollout{
+                ObjectMeta: isbServiceRollout.ObjectMeta,
+                Spec:       apiv1.ISBServiceRolloutSpec{},
+            })).To(HaveOccurred())
+        })
+    })
 })
