1e684b83706c5d9fca1a1772533be2d38d5057a7
dsimha <darshan_simha@intuit.com>
2024-06-10 18:39:21 -0400

    fix: updated the design to have details in the numarollout tab
    Signed-off-by: dsimha <darshan_simha@intuit.com>

--- extensions/argo/numa-rollout/numa-rollout-extension/ui/src/rollout/RolloutComponentWrapper.tsx
+++ extensions/argo/numa-rollout/numa-rollout-extension/ui/src/rollout/RolloutComponentWrapper.tsx
@@ -1,8 +1,14 @@
 import React, { useMemo } from "react";
 import { ArgoPropType, Node } from "../ArgoPropType";
+import { Box, Paper } from "@mui/material";
 
 export const RolloutComponentWrapper = (props: ArgoPropType) => {
   const currentNodeKind = props.resource.kind;
+  const currentStatusResource = props.application.status.resources.filter(
+    (resource) => {
+      return resource.kind === currentNodeKind;
+    }
+  );
   const nodeKindToNodeArrayMap = useMemo(() => {
     const nodeKindToNodeArrayMap = new Map<string, Node[]>();
     const tree = props.tree;
@@ -18,19 +24,48 @@ export const RolloutComponentWrapper = (props: ArgoPropType) => {
     return nodeKindToNodeArrayMap;
   }, [props.tree]);
   return (
-    <div>
-      <div>This is Numarollout Component for {currentNodeKind}</div>
-      {nodeKindToNodeArrayMap.get(currentNodeKind)?.map((node) => {
-        return (
-          <div key={node.uid}>
-            <div>Kind : {node.kind}</div>
-            <div>Name : {node.name}</div>
-            <div>Namespace : {node.namespace}</div>
-            <div>Resource Version : {node.resourceVersion}</div>
-            <div>Created at : {new Date(node.createdAt).toTimeString()}</div>
-          </div>
-        );
-      })}
-    </div>
+    <Box>
+      <h4>This is Numarollout Component for {currentNodeKind}</h4>
+      <Paper sx={{ padding: "1rem" }}>
+        <h5>Meta Data</h5>
+        {nodeKindToNodeArrayMap.get(currentNodeKind)?.map((node) => {
+          return (
+            <Box key={node.uid}>
+              <Box>Kind : {node.kind}</Box>
+              <Box>Name : {node.name}</Box>
+              <Box>Namespace : {node.namespace}</Box>
+              <Box>Resource Version : {node.resourceVersion}</Box>
+              <Box>Created at : {new Date(node.createdAt).toTimeString()}</Box>
+            </Box>
+          );
+        })}
+      </Paper>
+
+      <Paper sx={{ marginTop: "2rem", padding: "1rem" }}>
+        <h5>Status Data</h5>
+        <Box> Status : {currentStatusResource[0].status}</Box>
+        <Box>
+          {" "}
+          Repo URL :{" "}
+          <a href={props.application.spec.source.repoURL} target="_blank">
+            {props.application.spec.source.repoURL}
+          </a>
+        </Box>
+      </Paper>
+
+      <Paper sx={{ marginTop: "2rem", padding: "1rem" }}>
+        <h5>Revision Data</h5>
+        <Box>
+          {props.application.status.history.map((revision) => {
+            return (
+              <Box key={revision.id}>
+                <Box>Revision Hash : {revision.revision}</Box>
+                <Box>Deployed At : {revision.deployedAt}</Box>
+              </Box>
+            );
+          })}
+        </Box>
+      </Paper>
+    </Box>
   );
 };
