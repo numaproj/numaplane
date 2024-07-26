5b1c04739435bcd94075c5bceb9fcd1690049910
Julie Vogelman <julievogelman0@gmail.com>
2024-07-26 14:31:08 -0700

    save state
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -17,11 +17,21 @@ cd "${FAKE_REPOPATH}"
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${FAKE_REPOPATH}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-bash -x ${CODEGEN_PKG}/kube_codegen.sh "client" \
+chmod +x ${CODEGEN_PKG}/*.sh
+
+
+subheader "running codegen"
+bash -x ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/numaproj/numaplane/pkg/client github.com/numaproj/numaplane/pkg/apis \
   "numaplane:v1alpha1" \
-  --go-header-file hack/boilerplate.go.txt
+  --go-header-file hack/boilerplate/boilerplate.go.txt
+
+bash -x ${CODEGEN_PKG}/generate-groups.sh "client,informer,lister" \
+  github.com/numaproj/numaplane/pkg/client github.com/numaproj/numaplane/pkg/apis \
+  "numaplane:v1alpha1" \
+  --go-header-file hack/boilerplate/boilerplate.go.txt
 
 # gofmt the tree
 subheader "running gofmt"
 find . -name "*.go" -type f -print0 | xargs -0 gofmt -s -w
+
