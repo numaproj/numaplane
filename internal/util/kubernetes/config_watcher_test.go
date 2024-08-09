bda07b1d2ec15691d1f0fe5683485dc08c422e5b
Antonino Fugazzotto <antonino_fugazzotto@intuit.com>
2024-08-09 12:19:41 -0700

    added unit tests
    Signed-off-by: Antonino Fugazzotto <antonino_fugazzotto@intuit.com>

--- internal/util/kubernetes/config_watcher_test.go
+++ internal/util/kubernetes/config_watcher_test.go
@@ -28,6 +28,7 @@ func Test_watchConfigMaps(t *testing.T) {
 
 	data, err := os.ReadFile("../../../tests/config/controller-definitions-config.yaml")
 	assert.NoError(t, err)
+
 	// Create a new ConfigMap object
 	configMap := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
@@ -48,17 +49,62 @@ func Test_watchConfigMaps(t *testing.T) {
 
 	// Wait for the controller to process the ConfigMap
 	time.Sleep(5 * time.Second)
+
 	// Validate the controller definition config is set correctly
 	definition := config.GetConfigManagerInstance().GetControllerDefinitionsMgr().GetNumaflowControllerDefinitionsConfig()
 	assert.Len(t, definition, 2)
 
-	// Create the ConfigMap object in the fake clientset
+	// Delete the ConfigMap object from the fake clientset
 	err = clientSet.CoreV1().ConfigMaps("default").Delete(ctx, configMap.Name, metav1.DeleteOptions{})
 	assert.NoError(t, err)
 
 	// Wait for the controller to process the ConfigMap
 	time.Sleep(5 * time.Second)
-	// Validate the controller definition config is updated correctly
+
+	// Validate the controller definition config has been removed
 	definition = config.GetConfigManagerInstance().GetControllerDefinitionsMgr().GetNumaflowControllerDefinitionsConfig()
 	assert.Len(t, definition, 0)
+
+	// === USDE Config Test =====================================
+
+	configMap = &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "numaplane-controller-usde-config",
+			Namespace: "default",
+			Labels: map[string]string{
+				common.LabelKeyNumaplaneControllerConfig: "usde-config",
+			},
+		},
+		Data: map[string]string{
+			"pipelineSpecExcludedPaths": `
+        - "abc"
+        - "abcde/xyz"
+        - "path/array/sample"
+      `,
+			"isbServiceSpecExcludedPaths": `
+        - "invalid"
+      `,
+		},
+	}
+
+	_, err = clientSet.CoreV1().ConfigMaps("default").Create(ctx, configMap, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	time.Sleep(5 * time.Second)
+
+	expectedUSDEConfig := config.USDEConfig{
+		PipelineSpecExcludedPaths:   []string{"abc", "abcde/xyz", "path/array/sample"},
+		ISBServiceSpecExcludedPaths: []string{"invalid"},
+	}
+
+	actualUSDEConfig := config.GetConfigManagerInstance().GetUSDEConfig()
+	assert.Equal(t, expectedUSDEConfig, *actualUSDEConfig)
+
+	err = clientSet.CoreV1().ConfigMaps("default").Delete(ctx, configMap.Name, metav1.DeleteOptions{})
+	assert.NoError(t, err)
+
+	time.Sleep(5 * time.Second)
+
+	actualUSDEConfig = config.GetConfigManagerInstance().GetUSDEConfig()
+	assert.Equal(t, config.USDEConfig{}, *actualUSDEConfig)
 }
