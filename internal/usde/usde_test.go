f36a51c69cb3df4f26edeb757200ac1e49cb71e0
Antonino Fugazzotto <antonino_fugazzotto@intuit.com>
2024-08-27 17:05:41 -0700

    added more USDE test cases
    Signed-off-by: Antonino Fugazzotto <antonino_fugazzotto@intuit.com>

--- internal/usde/usde_test.go
+++ internal/usde/usde_test.go
@@ -15,12 +15,11 @@ import (
 	apiv1 "github.com/numaproj/numaplane/pkg/apis/numaplane/v1alpha1"
 )
 
-var pipelineSpecSourceRPU = int64(5)
-var pipelineSpecSourceDuration = metav1.Duration{
-	Duration: time.Second,
-}
+const defaultNamespace = "default"
 
-var pipelineSpec = numaflowv1.PipelineSpec{
+var pipelineSpecSourceRPU = int64(5)
+var pipelineSpecSourceDuration = metav1.Duration{Duration: 2 * time.Second}
+var defaultPipelineSpec = numaflowv1.PipelineSpec{
 	InterStepBufferServiceName: "my-isbsvc",
 	Vertices: []numaflowv1.AbstractVertex{
 		{
@@ -61,54 +60,172 @@ var pipelineSpec = numaflowv1.PipelineSpec{
 	},
 }
 
-var pipelineSpecRaw, _ = json.Marshal(pipelineSpec)
+func makePipelineDefinition(pipelineSpec numaflowv1.PipelineSpec) kubernetes.GenericObject {
+	pipelineSpecRaw, _ := json.Marshal(pipelineSpec)
 
-var prs = apiv1.PipelineRolloutSpec{
-	Pipeline: apiv1.Pipeline{
-		Spec: runtime.RawExtension{
-			Raw: pipelineSpecRaw,
+	prs := apiv1.PipelineRolloutSpec{
+		Pipeline: apiv1.Pipeline{
+			Spec: runtime.RawExtension{
+				Raw: pipelineSpecRaw,
+			},
 		},
-	},
-}
+	}
 
-var defaultNewSpec = kubernetes.GenericObject{
-	TypeMeta: metav1.TypeMeta{
-		Kind:       "Pipeline",
-		APIVersion: "numaflow.numaproj.io/v1alpha1",
-	},
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "test-pipeline",
-		Namespace: "default",
-	},
-	Spec: prs.Pipeline.Spec,
+	return kubernetes.GenericObject{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pipeline",
+			APIVersion: "numaflow.numaproj.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pipeline",
+			Namespace: defaultNamespace,
+		},
+		Spec: prs.Pipeline.Spec,
+	}
 }
-
-// var defaultExistingSpec kubernetes.GenericObject = kubernetes.GenericObject{}
 
 func Test_GetUpgradeStrategy(t *testing.T) {
 	configManager := config.GetConfigManagerInstance()
+
+	pipelineDefn := makePipelineDefinition(defaultPipelineSpec)
 
 	testCases := []struct {
 		name             string
 		newSpec          kubernetes.GenericObject
 		existingSpec     kubernetes.GenericObject
 		usdeConfig       config.USDEConfig
+		namespaceConfig  config.NamespaceConfig
 		expectedStrategy UpgradeStrategy
 	}{
 		{
 			name:         "empty pipeline spec excluded paths",
-			newSpec:      defaultNewSpec,
-			existingSpec: defaultNewSpec,
+			newSpec:      pipelineDefn,
+			existingSpec: pipelineDefn,
 			usdeConfig: config.USDEConfig{
 				PipelineSpecExcludedPaths: []string{},
 			},
+			namespaceConfig:  config.NamespaceConfig{},
 			expectedStrategy: UpgradeStrategyNoOp,
+		},
+		{
+			name:    "only exclude interStepBufferServiceName field (changed)",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.InterStepBufferServiceName = "changed-isbsvc"
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{},
+			expectedStrategy: UpgradeStrategyApply,
+		},
+		{
+			name:         "only exclude interStepBufferServiceName field (NOT changed)",
+			newSpec:      pipelineDefn,
+			existingSpec: pipelineDefn,
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{},
+			expectedStrategy: UpgradeStrategyNoOp,
+		},
+		{
+			name:    "only exclude interStepBufferServiceName field and change some other field (no user strategy)",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.Vertices[0].Name = "new-vtx-name"
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{},
+			expectedStrategy: UpgradeStrategyProgressive,
+		},
+		{
+			name:    "only exclude interStepBufferServiceName field and change some other field (with empty user strategy)",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.Vertices[0].Name = "new-vtx-name"
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{UpgradeStrategy: ""},
+			expectedStrategy: UpgradeStrategyProgressive,
+		},
+		{
+			name:    "only exclude interStepBufferServiceName field and change some other field (with invalid user strategy)",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.Vertices[0].Name = "new-vtx-name"
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{UpgradeStrategy: "invalid"},
+			expectedStrategy: UpgradeStrategyProgressive,
+		},
+		{
+			name:    "only exclude interStepBufferServiceName field and change some other field (with valid user strategy)",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.Vertices[0].Name = "new-vtx-name"
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName"},
+			},
+			namespaceConfig:  config.NamespaceConfig{UpgradeStrategy: "pause-and-drain"},
+			expectedStrategy: UpgradeStrategyPPND,
+		},
+		{
+			name:    "with changes in array deep map but excluded",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newRPU := int64(10)
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.InterStepBufferServiceName = "changed-isbsvc"
+				newPipelineSpec.Vertices[0].Source.Generator.RPU = &newRPU
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName", "vertices.source.generator.rpu"},
+			},
+			namespaceConfig:  config.NamespaceConfig{UpgradeStrategy: "pause-and-drain"},
+			expectedStrategy: UpgradeStrategyApply,
+		},
+		{
+			name:    "with changes in array deep map but one is NOT excluded",
+			newSpec: pipelineDefn,
+			existingSpec: func() kubernetes.GenericObject {
+				newRPU := int64(10)
+				newPipelineSpec := defaultPipelineSpec.DeepCopy()
+				newPipelineSpec.Vertices[0].Name = "new-vtx-name"
+				newPipelineSpec.InterStepBufferServiceName = "changed-isbsvc"
+				newPipelineSpec.Vertices[0].Source.Generator.RPU = &newRPU
+				return makePipelineDefinition(*newPipelineSpec)
+			}(),
+			usdeConfig: config.USDEConfig{
+				PipelineSpecExcludedPaths: []string{"interStepBufferServiceName", "vertices.source.generator.rpu"},
+			},
+			namespaceConfig:  config.NamespaceConfig{UpgradeStrategy: "pause-and-drain"},
+			expectedStrategy: UpgradeStrategyPPND,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			configManager.UpdateUSDEConfig(tc.usdeConfig)
+			configManager.UpdateNamespaceConfig(defaultNamespace, tc.namespaceConfig)
 			strategy, err := GetUpgradeStrategy(&tc.newSpec, &tc.existingSpec)
 			assert.NoError(t, err)
 			assert.Equal(t, tc.expectedStrategy, strategy)
