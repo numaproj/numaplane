6e91019da28d752fa02c04315cde0697b58e95aa
Antonino Fugazzotto <161530773+afugazzotto@users.noreply.github.com>
2024-09-05 14:53:36 -0700

    feat: USDE Comparison Logic (#227)
    Signed-off-by: Antonino Fugazzotto <antonino_fugazzotto@intuit.com>

--- internal/util/util.go
+++ internal/util/util.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"maps"
 	"reflect"
+	"sort"
+	"strings"
 )
 
-func StructToStruct(src any, dest any) error {
+// StructToStruct converts a struct type (src) into another (dst)
+func StructToStruct(src any, dst any) error {
 	jsonBytes, err := json.Marshal(src)
 	if err != nil {
 		return err
 	}
 
-	err = json.Unmarshal(jsonBytes, dest)
+	err = json.Unmarshal(jsonBytes, dst)
 	if err != nil {
 		return fmt.Errorf("failed to convert json %s: err=%s", string(jsonBytes), err)
 	}
@@ -21,62 +24,184 @@ func StructToStruct(src any, dest any) error {
 	return nil
 }
 
-// compare 2 maps for equality, ignoring any null values, empty maps, and empty arrays
-func CompareMapsIgnoringNulls(a map[string]interface{}, b map[string]interface{}) bool {
-	aNoNulls := make(map[string]interface{})
+// CompareMapsIgnoringNulls compares 2 maps for equality, ignoring any null values, empty maps, empty arrays, zero numerical values, and empty strings
+func CompareMapsIgnoringNulls(a map[string]any, b map[string]any) bool {
+	aNoNulls := make(map[string]any)
 	maps.Copy(aNoNulls, a)
 
-	bNoNulls := make(map[string]interface{})
+	bNoNulls := make(map[string]any)
 	maps.Copy(bNoNulls, b)
 
-	removeNullValuesFromJSONMap(aNoNulls)
-	removeNullValuesFromJSONMap(bNoNulls)
+	removeNullValuesFromMap(aNoNulls)
+	removeNullValuesFromMap(bNoNulls)
+
 	return reflect.DeepEqual(aNoNulls, bNoNulls)
 }
 
-// recursively remove any zero values from the map including null references, empty strings, numbers that are zero,
-// empty maps, and empty arrays from the map
-// (the types that we look for are the ones that json.Unmarshal() uses)
-func removeNullValuesFromJSONMap(m map[string]interface{}) bool {
+// removeNullValuesFromMap recursively removes any zero values from the map including:
+// null references, empty strings, numbers that are zero, empty maps, and empty arrays
+func removeNullValuesFromMap(m map[string]any) {
+	for key, val := range m {
+		switch typedVal := val.(type) {
+		case map[string]any:
+			removeNullValuesFromMap(typedVal)
 
-	for k, v := range m {
-		if v == nil {
-			delete(m, k)
-		} else if stringValue, ok := v.(string); ok {
-			if stringValue == "" {
-				delete(m, k)
+			if len(typedVal) == 0 {
+				delete(m, key)
 			}
-			// scalar numbers seem to use float64:
-		} else if floatValue, ok := v.(float64); ok {
-			if floatValue == 0 {
-				delete(m, k)
-			}
-		} else if boolValue, ok := v.(bool); ok {
-			if !boolValue {
-				delete(m, k)
-			}
-		} else if nestedMap, ok := v.(map[string]interface{}); ok {
-			removeNullValuesFromJSONMap(nestedMap)
-			if len(nestedMap) == 0 {
-				delete(m, k)
-			}
-		} else if nestedSlice, ok := v.([]interface{}); ok {
-			allMapsEmpty := true
-			for _, sliceElem := range nestedSlice {
-				if asMap, ok := sliceElem.(map[string]interface{}); ok {
-					removeNullValuesFromJSONMap(asMap)
-					if len(asMap) != 0 {
-						allMapsEmpty = false
+
+		case []any:
+			for i := 0; i < len(typedVal); i++ {
+				if elemMap, ok := typedVal[i].(map[string]any); ok {
+					removeNullValuesFromMap(elemMap)
+
+					if len(elemMap) == 0 {
+						typedVal = append(typedVal[:i], typedVal[i+1:]...)
+						i--
 					}
-				} else {
-					allMapsEmpty = false
+				} else if typedVal[i] == nil {
+					typedVal = append(typedVal[:i], typedVal[i+1:]...)
+					i--
 				}
 			}
-			if allMapsEmpty {
-				delete(m, k)
+
+			if len(typedVal) == 0 {
+				delete(m, key)
+			} else {
+				m[key] = typedVal
+			}
+
+		case nil:
+			delete(m, key)
+
+		default:
+			if reflect.ValueOf(typedVal).IsZero() {
+				delete(m, key)
 			}
 
 		}
 	}
-	return false
+}
+
+// SplitObject returns 2 maps from a given object as bytes array and a slice of paths.
+// One of the 2 output maps will include only the paths from the slice while the second returned map will include all other paths.
+func SplitObject(obj []byte, paths []string, pathSeparator string) (map[string]any, map[string]any, error) {
+	var objAsMap map[string]any
+	if err := json.Unmarshal(obj, &objAsMap); err != nil {
+		return nil, nil, err
+	}
+
+	return SplitMap(objAsMap, paths, pathSeparator)
+}
+
+// SplitMap returns 2 maps from a given map and a slice of paths.
+// One of the 2 output maps will include only the paths from the slice while the second returned map will include all other paths.
+func SplitMap(m map[string]any, paths []string, pathSeparator string) (onlyPaths map[string]any, withoutPaths map[string]any, err error) {
+	onlyPaths = make(map[string]any)
+	withoutPaths, err = cloneMap(m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// In case there are no paths, return an empty map and the original map.
+	// This is in place so that this logic can be bypassed at runtime by providing
+	// an empty slice of paths.
+	if len(paths) == 0 {
+		return onlyPaths, withoutPaths, nil
+	}
+
+	// Sort the paths slice before using it to avoid maps merging in case the slice were to
+	// include deeply nested fields and then the higher level fields (ex: []string{"map.field.inner2", "map"})
+	sort.Strings(paths)
+
+	for _, path := range paths {
+		pathTokens := strings.Split(path, pathSeparator)
+
+		if err := extractPath(withoutPaths, onlyPaths, pathTokens); err != nil {
+			return nil, nil, err
+		}
+	}
+
+	removeNullValuesFromMap(onlyPaths)
+	removeNullValuesFromMap(withoutPaths)
+
+	return onlyPaths, withoutPaths, nil
+}
+
+// cloneMap returns a clone of the given map
+func cloneMap(m map[string]any) (map[string]any, error) {
+	var clone map[string]any
+
+	if err := StructToStruct(m, &clone); err != nil {
+		return nil, err
+	}
+
+	return clone, nil
+}
+
+// extractPath extracts a path from the source map into the destination map based on a slice of token representing the path
+func extractPath(src, dst map[string]any, pathTokens []string) error {
+	// panic guardrail (this condition should never be reached and true)
+	if len(pathTokens) == 0 {
+		return nil
+	}
+
+	key := pathTokens[0]
+
+	srcVal, exists := src[key]
+	if !exists {
+		return nil
+	}
+
+	// Last path token sets the value or merges maps
+	if len(pathTokens) == 1 {
+		dst[key] = src[key]
+		delete(src, key)
+		return nil
+	}
+
+	switch nextSrc := srcVal.(type) {
+	case map[string]any:
+		if _, exists := dst[key]; !exists {
+			dst[key] = make(map[string]any)
+		}
+
+		if err := extractPath(nextSrc, dst[key].(map[string]any), pathTokens[1:]); err != nil {
+			return err
+		}
+
+	case []any:
+		if _, exists := dst[key]; !exists {
+			dst[key] = make([]any, len(nextSrc))
+		}
+
+		// Loop throught each slice element to extract paths inside slice of objects
+		for i := range nextSrc {
+			switch nextSrcElem := nextSrc[i].(type) {
+			case map[string]any:
+				nextDestArr := dst[key].([]any)
+				if nextDestArr[i] == nil {
+					nextDestArr[i] = make(map[string]any)
+				}
+
+				if err := extractPath(nextSrcElem, nextDestArr[i].(map[string]any), pathTokens[1:]); err != nil {
+					return err
+				}
+
+			case []any:
+				// TODO: this should not be necessary in this context (not many array of arrays in k8s yaml definitions),
+				// but implement it for completeness (low priority)
+
+			default:
+				dst[key] = nextSrc
+				return nil
+			}
+		}
+
+	default:
+		dst[key] = srcVal
+		delete(src, key)
+	}
+
+	return nil
 }
