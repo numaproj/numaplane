6047a106b81fd94b3afd057fd50bfe164dc6e768
Dillen Padhiar <dillen_padhiar@intuit.com>
2024-09-17 10:19:19 -0700

    test: enable debuggability for e2e tests
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- tests/e2e/common.go
+++ tests/e2e/common.go
@@ -3,6 +3,8 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -19,6 +21,7 @@ import (
 	"github.com/numaproj/numaplane/internal/util/kubernetes"
 	apiv1 "github.com/numaproj/numaplane/pkg/apis/numaplane/v1alpha1"
 	planepkg "github.com/numaproj/numaplane/pkg/client/clientset/versioned/typed/numaplane/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 var (
@@ -41,6 +44,12 @@ var (
 
 const (
 	Namespace = "numaplane-system"
+
+	NumaplaneCtrlLogs = "output/numaplane-controller.log"
+	NumaflowCtrlLogs  = "output/numaflow-controller.log"
+
+	NumaplaneLabel = "app.kubernetes.io/part-of=numaplane"
+	NumaflowLabel  = "app.kubernetes.io/part-of=numaflow, app.kubernetes.io/component=controller-manager"
 )
 
 // document for Ginkgo framework and print to console
@@ -107,3 +116,33 @@ func printPodLogs(client clientgo.Interface, namespace, podName, containerName s
 	logBytes, _ := io.ReadAll(stream)
 	fmt.Printf("Printing Log for namespace=%q, pod=%q, container=%q:\n%s\n", namespace, podName, containerName, string(logBytes))
 }*/
+
+func getPodLogs(client clientgo.Interface, namespace, labelSelector, containerName, fileName string) {
+
+	ctx := context.Background()
+	podLogOptions := &corev1.PodLogOptions{Container: containerName}
+
+	podList, err := client.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		// TODO: improve error reporting
+		fmt.Println("Error getting pods logs")
+		return
+	}
+
+	for _, pod := range podList.Items {
+		stream, err := client.CoreV1().Pods(namespace).GetLogs(pod.Name, podLogOptions).Stream(ctx)
+		if err != nil {
+			fmt.Println("Error getting pods logs")
+			return
+		}
+		defer stream.Close()
+		logBytes, _ := io.ReadAll(stream)
+
+		err = os.WriteFile(fileName, logBytes, 0644)
+		if err != nil {
+			fmt.Println("Error writing pod logs")
+			return
+		}
+	}
+
+}
