196a94e503f6c65880325707acdbabb4387221b6
Julie Vogelman <julievogelman0@gmail.com>
2024-10-06 17:54:45 -0700

    numaflowcontrollerrollout unit test
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- internal/controller/numaflowcontrollerrollout_controller_test.go
+++ internal/controller/numaflowcontrollerrollout_controller_test.go
@@ -31,10 +31,29 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	numaflowv1 "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
+	"github.com/numaproj/numaplane/internal/controller/config"
+	"github.com/numaproj/numaplane/internal/util/kubernetes"
+	"github.com/numaproj/numaplane/internal/util/metrics"
 	apiv1 "github.com/numaproj/numaplane/pkg/apis/numaplane/v1alpha1"
+	commontest "github.com/numaproj/numaplane/tests/common"
 )
+
+func createNumaflowControllerRolloutDef(namespace string, version string) apiv1.NumaflowControllerRollout {
+	return apiv1.NumaflowControllerRollout{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      NumaflowControllerDeploymentName,
+			Namespace: namespace,
+		},
+		Spec: apiv1.NumaflowControllerRolloutSpec{
+			Controller: apiv1.Controller{Version: version},
+		},
+	}
+}
 
 var _ = Describe("NumaflowControllerRollout Controller", Ordered, func() {
 	const namespace = "default"
@@ -47,15 +66,7 @@ var _ = Describe("NumaflowControllerRollout Controller", Ordered, func() {
 			Namespace: namespace,
 		}
 
-		numaflowControllerResource := apiv1.NumaflowControllerRollout{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      NumaflowControllerDeploymentName,
-				Namespace: namespace,
-			},
-			Spec: apiv1.NumaflowControllerRolloutSpec{
-				Controller: apiv1.Controller{Version: "1.2.0"},
-			},
-		}
+		numaflowControllerResource := createNumaflowControllerRolloutDef(defaultNamespace, "1.2.0")
 
 		It("Should throw a CR validation error", func() {
 			By("Creating a NumaflowControllerRollout resource with an invalid name")
@@ -309,4 +320,84 @@ func Test_getControllerDeploymentVersion(t *testing.T) {
 		})
 	}
 
+}
+
+func Test_reconcile_numaflowcontrollerrollout_PPND(t *testing.T) {
+
+	restConfig, numaflowClientSet, numaplaneClient, k8sClientSet, err := commontest.PrepareK8SEnvironment()
+	assert.Nil(t, err)
+
+	config.GetConfigManagerInstance().UpdateUSDEConfig(config.USDEConfig{DefaultUpgradeStrategy: config.PPNDStrategyID})
+	config.GetConfigManagerInstance().GetControllerDefinitionsMgr().UpdateNumaflowControllerDefinitionConfig(getNumaflowControllerDefinitions())
+
+	ctx := context.Background()
+
+	// other tests may call this, but it fails if called more than once
+	if customMetrics == nil {
+		customMetrics = metrics.RegisterCustomMetrics()
+	}
+
+	recorder := record.NewFakeRecorder(64)
+
+	numaflowControllerReconciler, err := NewNumaflowControllerRolloutReconciler(
+		numaplaneClient,
+		scheme.Scheme,
+		restConfig,
+		kubernetes.NewKubectl(),
+		customMetrics,
+		recorder,
+	)
+
+	testCases := []struct {
+		name                      string
+		newControllerVersion      string
+		existingControllerVersion *string // if nil, then there isn't a Controller installed
+		stillReconciling          bool
+		existingPipeline          *numaflowv1.Pipeline
+		expectedRolloutPhase      apiv1.Phase
+		// require these Conditions to be set (note that in real life, previous reconciliations may have set other Conditions from before which are still present)
+		expectedConditionsSet           map[apiv1.ConditionType]metav1.ConditionStatus
+		expectedResultControllerVersion string
+	}{}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			// first delete previous resources in case they already exist, in Kubernetes
+			_ = k8sClientSet.AppsV1().Deployments(defaultNamespace).DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{})
+			_ = k8sClientSet.CoreV1().ConfigMaps(defaultNamespace).DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{})
+			_ = k8sClientSet.RbacV1().Roles(defaultNamespace).DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{})
+			_ = k8sClientSet.RbacV1().RoleBindings(defaultNamespace).DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{})
+
+			// create NumaflowControllerRollout definition
+			rollout := createNumaflowControllerRolloutDef(defaultNamespace, tc.newControllerVersion)
+
+			// the Reconcile() function does this, so we need to do it before calling reconcile() as well
+			rollout.Status.Init(rollout.Generation)
+
+			// create the already-existing Deployment in Kubernetes
+			if tc.existingControllerVersion != nil {
+				imagePath := "numaflow:v" + *tc.existingControllerVersion
+				k8sClientSet.AppsV1().Deployments(defaultNamespace).Create(ctx, createDeploymentDefinition(imagePath), metav1.CreateOptions{})
+			}
+		})
+	}
+
+}
+
+func createDeploymentDefinition(imagePath string) *appsv1.Deployment {
+	return &appsv1.Deployment{
+		Spec: appsv1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "main",
+							Image: imagePath,
+						},
+					},
+				},
+			},
+		},
+	}
 }
