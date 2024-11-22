78f003f6ac2e591a34b18ef3625013427c241965
Dillen Padhiar <dillen_padhiar@intuit.com>
2024-11-21 16:53:40 -0800

    chore: add consistency with labels by adding  labels to defaultPipeline method
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- internal/controller/pipelinerollout/pipelinerollout_controller_test.go
+++ internal/controller/pipelinerollout/pipelinerollout_controller_test.go
@@ -489,7 +489,8 @@ func TestBasePipelineMetadata(t *testing.T) {
 }
 
 func createDefaultTestPipeline(phase numaflowv1.PipelinePhase) *numaflowv1.Pipeline {
-	return ctlrcommon.CreateTestPipelineOfSpec(pipelineSpec, ctlrcommon.DefaultTestPipelineName, phase, numaflowv1.Status{}, false, map[string]string{})
+	return ctlrcommon.CreateTestPipelineOfSpec(pipelineSpec, ctlrcommon.DefaultTestPipelineName, phase, numaflowv1.Status{}, false,
+		map[string]string{"numaplane.numaproj.io/isbsvc-name": "my-isbsvc", "numaplane.numaproj.io/parent-rollout-name": "pipelinerollout-test", "numaplane.numaproj.io/upgrade-state": "promoted"})
 }
 
 func createPipeline(phase numaflowv1.PipelinePhase, status numaflowv1.Status, drainedOnPause bool, labels map[string]string) *numaflowv1.Pipeline {
