f48ec08b365f6a453fd9c6b030ea39bb758c7098
Julie Vogelman <julievogelman0@gmail.com>
2025-01-10 12:16:47 -0800

    add numaplane clientset
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- internal/util/kubernetes/clientset.go
+++ internal/util/kubernetes/clientset.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"fmt"
 
+	"github.com/numaproj/numaplane/pkg/client/clientset/versioned"
 	"k8s.io/client-go/dynamic"
 	clientkube "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -10,6 +11,7 @@ import (
 
 var DynamicClient *dynamic.DynamicClient
 var KubernetesClient *clientkube.Clientset
+var NumaplaneClient *versioned.Clientset
 
 func SetClientSets(restConfig *rest.Config) error {
 	var err error
@@ -20,7 +22,12 @@ func SetClientSets(restConfig *rest.Config) error {
 
 	KubernetesClient, err = clientkube.NewForConfig(restConfig)
 	if err != nil {
-		return fmt.Errorf("failed to create kubernetes client: %v", err)
+		return fmt.Errorf("failed to create kubernetes clientset: %v", err)
+	}
+
+	NumaplaneClient, err = versioned.NewForConfig(restConfig)
+	if err != nil {
+		return fmt.Errorf("failed to create numaplane clientset: %v", err)
 	}
 
 	return nil
