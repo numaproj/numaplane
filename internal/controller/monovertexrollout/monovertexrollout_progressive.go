18c5658238aec19399c85bd415bb443c48fe934c
Antonino Fugazzotto <antonino_fugazzotto@intuit.com>
2025-02-13 13:27:58 -0800

    removed check for actualPodsCount == 0
    Signed-off-by: Antonino Fugazzotto <antonino_fugazzotto@intuit.com>

--- internal/controller/monovertexrollout/monovertexrollout_progressive.go
+++ internal/controller/monovertexrollout/monovertexrollout_progressive.go
@@ -198,11 +198,6 @@ func scaleDownMonoVertex(
 
 	actualPodsCount := int64(len(podsList.Items))
 
-	// If there are no pods, return and do not requeue
-	if actualPodsCount == 0 {
-		return false, nil
-	}
-
 	// If for the vertex we already set a Scaled scale value, we only need to update the actual pods count
 	// to later verify that the pods were actually scaled down.
 	// We want to skip scaling down again.
