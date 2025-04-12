bc2a1d8681d829c7b709cbe38969971734c70a89
Dillen Padhiar <38965141+dpadhiar@users.noreply.github.com>
2025-04-12 16:39:51 -0700

    feat: AnalysisRun implementation - unit tests  (#695)
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- internal/controller/progressive/progressive_test.go
+++ internal/controller/progressive/progressive_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	argorolloutsv1 "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	numaflowv1 "github.com/numaproj/numaflow/pkg/apis/numaflow/v1alpha1"
 	"github.com/numaproj/numaplane/internal/common"
 	ctlrcommon "github.com/numaproj/numaplane/internal/controller/common"
@@ -43,9 +44,9 @@ func (fpc fakeProgressiveController) ChildNeedsUpdating(ctx context.Context, exi
 
 func (fpc fakeProgressiveController) AssessUpgradingChild(ctx context.Context, rolloutObject ProgressiveRolloutObject, existingUpgradingChildDef *unstructured.Unstructured) (apiv1.AssessmentResult, string, error) {
 	switch existingUpgradingChildDef.GetName() {
-	case "test-success":
+	case "test-success", "test-analysis-success":
 		return apiv1.AssessmentResultSuccess, "", nil
-	case "test-failure":
+	case "test-failure", "test-analysis-failure":
 		return apiv1.AssessmentResultFailure, "test-fail-reason", nil
 	default:
 		return apiv1.AssessmentResultUnknown, "", nil
@@ -203,6 +204,60 @@ func Test_processUpgradingChild(t *testing.T) {
 			expectedRequeueDelay:      0,
 			expectedError:             nil,
 		},
+		{
+			name: "analysis status set - success",
+			rolloutObject: setMonoVertexProgressiveStatus(
+				analysisTmplMonoVertexRollout.DeepCopy(),
+				&apiv1.UpgradingMonoVertexStatus{
+					UpgradingPipelineTypeStatus: apiv1.UpgradingPipelineTypeStatus{
+						UpgradingChildStatus: apiv1.UpgradingChildStatus{
+							Name:                "test-analysis-success",
+							AssessmentResult:    apiv1.AssessmentResultUnknown,
+							AssessmentStartTime: &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							AssessmentEndTime:   &metav1.Time{Time: time.Now()},
+						},
+						Analysis: apiv1.AnalysisStatus{
+							AnalysisRunName: "test-analysis-success",
+							StartTime:       &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							EndTime:         &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							Phase:           argorolloutsv1.AnalysisPhaseSuccessful,
+						},
+					},
+				},
+				nil,
+			),
+			existingUpgradingChildDef: createMonoVertex("test-analysis-success"),
+			expectedDone:              true,
+			expectedRequeueDelay:      0,
+			expectedError:             nil,
+		},
+		{
+			name: "analysis status set - failure",
+			rolloutObject: setMonoVertexProgressiveStatus(
+				analysisTmplMonoVertexRollout.DeepCopy(),
+				&apiv1.UpgradingMonoVertexStatus{
+					UpgradingPipelineTypeStatus: apiv1.UpgradingPipelineTypeStatus{
+						UpgradingChildStatus: apiv1.UpgradingChildStatus{
+							Name:                "test-analysis-failure",
+							AssessmentResult:    apiv1.AssessmentResultUnknown,
+							AssessmentStartTime: &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							AssessmentEndTime:   &metav1.Time{Time: time.Now()},
+						},
+						Analysis: apiv1.AnalysisStatus{
+							AnalysisRunName: "test-analysis-failure",
+							StartTime:       &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							EndTime:         &metav1.Time{Time: time.Now().Add(-1 * time.Minute)},
+							Phase:           argorolloutsv1.AnalysisPhaseFailed,
+						},
+					},
+				},
+				nil,
+			),
+			existingUpgradingChildDef: createMonoVertex("test-analysis-failure"),
+			expectedDone:              false,
+			expectedRequeueDelay:      0,
+			expectedError:             nil,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -335,6 +390,29 @@ var forcePromoteMonoVertexRollout = &apiv1.MonoVertexRollout{
 	},
 }
 
+var analysisTmplMonoVertexRollout = &apiv1.MonoVertexRollout{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "test",
+	},
+	Status: apiv1.MonoVertexRolloutStatus{
+		ProgressiveStatus: apiv1.MonoVertexProgressiveStatus{
+			UpgradingMonoVertexStatus: nil,
+			PromotedMonoVertexStatus:  nil,
+		},
+	},
+	Spec: apiv1.MonoVertexRolloutSpec{
+		Strategy: &apiv1.PipelineTypeRolloutStrategy{
+			PipelineTypeProgressiveStrategy: apiv1.PipelineTypeProgressiveStrategy{
+				Analysis: apiv1.Analysis{
+					Templates: []argorolloutsv1.AnalysisTemplateRef{
+						{TemplateName: "test", ClusterScope: false},
+					},
+				},
+			},
+		},
+	},
+}
+
 func Test_CalculateScaleMinMaxValues(t *testing.T) {
 	testCases := []struct {
 		name           string
@@ -398,6 +476,49 @@ func Test_CalculateScaleMinMaxValues(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_getAnalysisRunTimeout(t *testing.T) {
+
+	testCases := []struct {
+		name            string
+		configToTest    string
+		expectedTimeout time.Duration
+		expectedErr     bool
+	}{
+		{
+			name:            "default timeout",
+			configToTest:    "testconfig",
+			expectedTimeout: time.Duration(1200) * time.Second,
+			expectedErr:     false,
+		},
+		{
+			name:            "custom timeout",
+			configToTest:    "testconfig2",
+			expectedTimeout: time.Duration(600) * time.Second,
+			expectedErr:     false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			getwd, err := os.Getwd()
+			assert.Nil(t, err, "Failed to get working directory")
+			configPath := filepath.Join(getwd, "../../../", "tests", "config")
+			configManager := config.GetConfigManagerInstance()
+			err = configManager.LoadAllConfigs(func(err error) {}, config.WithConfigsPath(configPath), config.WithConfigFileName(tc.configToTest))
+			assert.NoError(t, err)
+
+			analysisRunTimeout, err := getAnalysisRunTimeout(context.Background())
+			if tc.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedTimeout, analysisRunTimeout)
+			}
+		})
+	}
+
 }
 
 func Test_getChildStatusAssessmentSchedule(t *testing.T) {
