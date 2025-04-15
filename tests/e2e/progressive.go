8539267e108ddbad195c5b1381080593e4c5600c
Julie Vogelman <julievogelman0@gmail.com>
2025-04-15 09:42:56 -0700

    fix compile issue
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- tests/e2e/progressive.go
+++ tests/e2e/progressive.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 
-	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
