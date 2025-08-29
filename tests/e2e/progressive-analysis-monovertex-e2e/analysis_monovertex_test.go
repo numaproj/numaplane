40fdc1991e7f8dce36e9c323e8841b6a5c95c3ab
Julie Vogelman <julie_vogelman@intuit.com>
2025-08-29 16:38:23 -0700

    testing with earlier start times
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- tests/e2e/progressive-analysis-monovertex-e2e/analysis_monovertex_test.go
+++ tests/e2e/progressive-analysis-monovertex-e2e/analysis_monovertex_test.go
@@ -50,7 +50,7 @@ var (
 	defaultStrategy = apiv1.PipelineTypeRolloutStrategy{
 		PipelineTypeProgressiveStrategy: apiv1.PipelineTypeProgressiveStrategy{
 			Progressive: apiv1.ProgressiveStrategy{
-				AssessmentSchedule: "60,180,30,10",
+				AssessmentSchedule: "10,180,30,10",
 			},
 			Analysis: apiv1.Analysis{
 				Templates: []argov1alpha1.AnalysisTemplateRef{
