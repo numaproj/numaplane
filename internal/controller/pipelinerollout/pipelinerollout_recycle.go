18b4f765a0855b5a20a40ac54ef651b766873ba0
Julie Vogelman <julie_vogelman@intuit.com>
2025-11-01 09:40:24 -0700

    recycle retries
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- internal/controller/pipelinerollout/pipelinerollout_recycle.go
+++ internal/controller/pipelinerollout/pipelinerollout_recycle.go
@@ -89,6 +89,8 @@ func (r *PipelineRolloutReconciler) Recycle(
 		return true, err
 	}
 
+	// Get live Pipeline here
+
 	// First check if the PipelineRollout is configured to run
 	// If it's configured to be paused or has Vertex.scale.max==0, then we must respect the user's preference not to run
 	// TODO: reduce the number of Pipelines which are in this state by deleting any pipeline which has always been paused or scaled to 0 its entire life, in which case there's
