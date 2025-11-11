b5b1e1435a98446b694a28f4d8f2871a8b563fca
chandankumar4 <chandan.kr404@gmail.com>
2025-11-11 13:58:17 +0530

    enable isbservice metrics
    Signed-off-by: chandankumar4 <chandan.kr404@gmail.com>

--- internal/controller/isbservicerollout/isbservicerollout_controller.go
+++ internal/controller/isbservicerollout/isbservicerollout_controller.go
@@ -271,7 +271,7 @@ func (r *ISBServiceRolloutReconciler) reconcile(ctx context.Context, isbServiceR
 				}
 
 				isbServiceRollout.Status.MarkDeployed(isbServiceRollout.Generation)
-				//r.customMetrics.IncISBSvcProgressiveResults(newISBServiceDef.GetName(), isbServiceRollout, "false")
+				r.customMetrics.IncISBSvcProgressiveResults(newISBServiceDef.GetName(), isbServiceRollout, "false")
 				r.customMetrics.ReconciliationDuration.WithLabelValues(ControllerISBSVCRollout, "create").Observe(time.Since(startTime).Seconds())
 
 				// if there are any PipelineRollouts using this ISBServiceRollout, enqueue them for reconciliation
@@ -318,7 +318,7 @@ func (r *ISBServiceRolloutReconciler) reconcile(ctx context.Context, isbServiceR
 		}
 	}
 
-	//r.customMetrics.IncISBSvcProgressiveResults(newISBServiceDef.GetName(), isbServiceRollout, "true")
+	r.customMetrics.IncISBSvcProgressiveResults(newISBServiceDef.GetName(), isbServiceRollout, "true")
 	if requeueDelay > 0 {
 		return ctrl.Result{RequeueAfter: requeueDelay}, nil
 	}
