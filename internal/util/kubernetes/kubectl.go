257a1e83db25e1dabf03fcc00166e2787741cd85
Julie Vogelman <julievogelman0@gmail.com>
2026-06-15 14:10:35 -0700

    fix: force gitops-engine to use BearerTokenFile rather than BearerToken (cached file) for authenticating to Kubernetes API (#1000)
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- internal/util/kubernetes/kubectl.go
+++ internal/util/kubernetes/kubectl.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/utils/kube"
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/utils/tracing"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/util/openapi"
 
 	"github.com/numaproj/numaplane/internal/util/logger"
 )
@@ -17,6 +19,16 @@ func init() {
 	}
 }
 
+type kubectlCmd struct {
+	*kube.KubectlCmd
+}
+
+func (k *kubectlCmd) ManageResources(config *rest.Config, openAPISchema openapi.Resources) (kube.ResourceOperations, func(), error) {
+	return k.KubectlCmd.ManageResources(RestConfigForTempKubeconfig(config), openAPISchema)
+}
+
 func NewKubectl() kube.Kubectl {
-	return &kube.KubectlCmd{Tracer: tracer, Log: *logger.New().LogrLogger}
+	return &kubectlCmd{
+		KubectlCmd: &kube.KubectlCmd{Tracer: tracer, Log: *logger.New().LogrLogger},
+	}
 }
